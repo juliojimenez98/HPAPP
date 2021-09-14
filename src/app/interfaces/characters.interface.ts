15743ee197e14d610e757cfe52c9e40b5c95a208
juliojimenez98 <juliojimenez1998@gmail.com>
2021-09-14 11:56:51 -0300

    add service

---
+++ src/app/interfaces/characters.interface.ts
@@ -0,0 +1,36 @@
+export interface Welcome {
+  name:            string;
+  species:         Species;
+  gender:          Gender;
+  house:           string;
+  dateOfBirth:     string;
+  yearOfBirth:     number | string;
+  ancestry:        string;
+  eyeColour:       string;
+  hairColour:      string;
+  wand:            Wand;
+  patronus:        string;
+  hogwartsStudent: boolean;
+  hogwartsStaff:   boolean;
+  actor:           string;
+  alive:           boolean;
+  image:           string;
+}
+
+export enum Gender {
+  Female = "female",
+  Male = "male",
+}
+
+export enum Species {
+  Cat = "cat",
+  HalfGiant = "half-giant",
+  Human = "human",
+  Werewolf = "werewolf",
+}
+
+export interface Wand {
+  wood:   string;
+  core:   string;
+  length: number | string;
+}
--- src/app/services/hp.service.ts
+++ src/app/services/hp.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HpService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getCharacters():Observable<CharacterData[]>{
+    return this.http.get<CharacterData[]>('http://hp-api.herokuapp.com/api/characters')
+  }
 }
