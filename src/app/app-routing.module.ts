f943c669b1b6068f49dda1b5454f5290dbfdd236
juliojimenez98 <juliojimenez1998@gmail.com>
2021-09-14 14:08:49 -0300

    Add cards and houses colors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './hp/home/home.component';
 
 const routes: Routes = [
-{path:'', component:HomeComponent}
+{path:'', component:HomeComponent, pathMatch:'full'}
 ];
 
 @NgModule({
